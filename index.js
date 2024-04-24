d0fa588bb4c451657a4e81b1052ea61c42a288cb
Devin Lehmacher <lehmacdj@gmail.com>
2024-04-23 22:24:39 -0400

    fix missing translation

--- index.js
+++ index.js
@@ -1,52 +1,75 @@
-var popover = function(name, description, team) {
-  return '<button type="button" class="btn btn-default '+team.split(' ')[0]+'" href="#" data-content="'
-    + quoteattr(description)
-    + '" rel="popover" data-placement="top" data-original-title="'
-    + quoteattr(name + ' - ' + team)
-    + '" data-toggle="popover" data-trigger="hover">'
-    + quoteattr(name)
-    + '<\/button>';
+var popover = function (name, description, team) {
+  return (
+    '<button type="button" class="btn btn-default ' +
+    team.split(" ")[0] +
+    '" href="#" data-content="' +
+    quoteattr(description) +
+    '" rel="popover" data-placement="top" data-original-title="' +
+    quoteattr(name + " - " + team) +
+    '" data-toggle="popover" data-trigger="hover">' +
+    quoteattr(name) +
+    "</button>"
+  );
 };
 
-var tabPlaceholder = function(tierID) {
-  var id = 'tab-placeholder-' + tierID;
+var tabPlaceholder = function (tierID) {
+  var id = "tab-placeholder-" + tierID;
   return {
-    html: '<div id="' + id + '"><\/div>',
-    id: id
+    html: '<div id="' + id + '"></div>',
+    id: id,
   };
-}
-
-var tab = function(tier, active) {
-  return '<li class="' + (active ? 'active' : '') + '">'
-    + '<a href="#tier-' + quoteattr(tier.version) + '" data-toggle="tab" '
-    + 'aria-expanded="'
-    + (active ? 'true' : 'false') + '">'
-    + quoteattr(tier.name)
-    + '<\/a><\/li>';
 };
 
-var tierList = function(tier, active) {
-  return '<div class="tab-pane fade' + (active ? ' active in' : '')
-    + '" id="tier-' + quoteattr(tier.version) + '">'
-    + '<p>' + quoteattr(tier.description) + '<\/p>'
-    + '<div id="tier-' + quoteattr(tier.version) + '-list" class="container">'
-    + '<\/div>'
-    + '<p><a href="/detail?' + tier.version + '">Expand role descriptions.</a></p>'
-    + '<\/div>';
-}
+var tab = function (tier, active) {
+  return (
+    '<li class="' +
+    (active ? "active" : "") +
+    '">' +
+    '<a href="#tier-' +
+    quoteattr(tier.version) +
+    '" data-toggle="tab" ' +
+    'aria-expanded="' +
+    (active ? "true" : "false") +
+    '">' +
+    quoteattr(tier.name) +
+    "</a></li>"
+  );
+};
 
-var makeList = function(tierID, allRoles, active) {
+var tierList = function (tier, active) {
+  return (
+    '<div class="tab-pane fade' +
+    (active ? " active in" : "") +
+    '" id="tier-' +
+    quoteattr(tier.version) +
+    '">' +
+    "<p>" +
+    quoteattr(tier.description) +
+    "</p>" +
+    '<div id="tier-' +
+    quoteattr(tier.version) +
+    '-list" class="container">' +
+    "</div>" +
+    '<p><a href="/detail?' +
+    tier.version +
+    '">Ampliar las descripciones de funciones.</a></p>' +
+    "</div>"
+  );
+};
+
+var makeList = function (tierID, allRoles, active) {
   // in order to avoid out of order initialization of tabs we put a placeholder
   // before initializing it with the correct data
   var placeholder = tabPlaceholder(tierID);
-  $('#tier-tabs').append(placeholder.html);
-  $.getJSON(prefix + '/' + tierFileFor(tierID), function(tier) {
-    var roles = tier.roles.flatMap(x => toArray(getRoleData(allRoles, x)));
-    $('#' + placeholder.id).replaceWith(tab(tier, active));
-    $('#tiers').append(tierList(tier, active));
-    $.each(roles, function(ix, e) {
-      $('#tier-' + tier.version + '-list')
-        .append(popover(e.name, e.description, e.team));
+  $("#tier-tabs").append(placeholder.html);
+  $.getJSON(prefix + "/" + tierFileFor(tierID), function (tier) {
+    var roles = tier.roles.flatMap((x) => toArray(getRoleData(allRoles, x)));
+    $("#" + placeholder.id).replaceWith(tab(tier, active));
+    $("#tiers").append(tierList(tier, active));
+    $.each(roles, function (ix, e) {
+      $("#tier-" + tier.version + "-list").append(
+        popover(e.name, e.description, e.team)
+      );
     });
     // make popovers actually into popovers
     // Note: we need to do this initialization here because otherwise the
@@ -55,10 +78,10 @@ var makeList = function(tierID, allRoles, active) {
   });
 };
 
-$.getJSON(prefix + '/tier-manifest.json', function(tiers) {
-  $.getJSON(prefix + '/roles.json', function(roles) {
+$.getJSON(prefix + "/tier-manifest.json", function (tiers) {
+  $.getJSON(prefix + "/roles.json", function (roles) {
     // generate all of the tier lists
-    $.each(tiers, function(ix, e) {
+    $.each(tiers, function (ix, e) {
       var active = ix === 0;
       makeList(e, roles, active);
     });
